2f04f3347a5d89774459d25f1163027c4722db9b
Arnaldo Pangia <arnaldopangia@Federicas-Mac-mini.local>
2025-10-28 10:57:01 +0100

    fix mobile responsive design. Tablet design to be added. edited the footer

--- app.py
+++ app.py
@@ -98,7 +98,12 @@ def seinfeld_opening():
         },
     )
 
+# if __name__ == "__main__":
+#     import os
+#     port = int(os.environ.get("PORT", 80))
+#     app.run(host="0.0.0.0", port=port)
+
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 80))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
